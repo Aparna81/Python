7b8ad33f97dd622453d895bf2613e7759fd438c4
Aparna81 <140056693+Aparna81@users.noreply.github.com>
2025-06-10 13:59:31 +0000

    prime

--- Day_1/prime.py
+++ Day_1/prime.py
@@ -1,11 +1,22 @@
+print("start and end range")
 n = int(input())
-i=2
-c=0
-while(i<n):
-    if(n%i==0):
-        c+=1
-    i=i+1
-if(c==0):
-    print("it is a prime no" + str(i))
-else:
-    print("not prime")
+m = int(input())
+#i=2
+#c=0       check prime or not for a no
+#while(i<n):
+ #   if(n%i==0):
+  #      c+=1
+   # i=i+1
+#if(c==0):
+ #   print("it is a prime no" + str(i))
+#else:
+ #   print("not prime")
+for i in range(n,m+1):
+    j=2
+    c=0
+    while(j<i):
+        if(i%j==0):
+            c+=1
+        j+=1
+    if(c==0):
+        print(i)
