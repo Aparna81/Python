fd65e305d20886a94d00dcb2f3c0988a3ff2b354
Santhosh Pandi <109999710+santhoshpandi@users.noreply.github.com>
2025-06-12 16:39:45 +0530

    Update palindrome.py

--- Basic/palindrome.py
+++ Basic/palindrome.py
@@ -10,3 +10,28 @@ if(c==0):
     print("palidrome")
 else:
     print("not palindrome")
+
+
+'''
+
+Try to Optimize the program
+
+for example
+
+below is the optimized code
+
+s = input("Enter the String")
+a = len(s)-1
+c=0
+for i in s:
+    if i != s[a]:
+        c+=1
+        break
+    a-=1
+if c==0:
+    print("Pallindrome")
+else:
+    print("Not Pallindrome")
+
+'''
+    
