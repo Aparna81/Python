91c251e6ed074246cc36af3fed6b63d45ddc5305
Santhosh Pandi <109999710+santhoshpandi@users.noreply.github.com>
2025-06-12 16:47:32 +0530

    Update vowels.py

--- Basic/vowels.py
+++ Basic/vowels.py
@@ -11,3 +11,21 @@ while(i<len(s)):
         j+=1
     i+=1
 print(c)
+
+'''
+s is already in the type of 'string'
+
+there is no need to put like 'str(s[i])' instead write 's[i]'
+
+try to use 'in' keyword
+
+s = input('Enter the string')
+n = 'aeiou'
+count = 0
+for i in s:
+    if i in n:
+        count+=1
+print(f' The vowels count is {count}')
+    
+
+'''
